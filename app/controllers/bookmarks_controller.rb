e53cddcaa6ca696e2069423407d8366667584b63
Nirmala Luwang <nirmalaluwang@gmail.com>
2021-05-28 17:36:24 +0530

    2nd commit

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,34 +1,34 @@
 class BookmarksController < ApplicationController
 
-  def index
-    @bookmarks = Bookmark.all
-  end
+  # def index
+  #   @bookmarks = Bookmark.all
+  # end
 
-  def new
-    @bookmark = Bookmark.new
-  end
+  # def new
+  #   @bookmark = Bookmark.new
+  # end
 
-  def create
-    @movie = Movie.find(params[:movie_id])
-    @bookmarks = Bookmark.where(id: params[:movie_bookmark][:bookmark])
+  # def create
+  #   @movie = Movie.find(params[:movie_id])
+  #   @bookmarks = Bookmark.where(id: params[:movie_bookmark][:bookmark])
 
-    @bookmarks.each do |bookmark|
-      @movie_bookmark = MovieTag.create(movie: @movie, bookmark: bookmark)
-    end
+  #   @bookmarks.each do |bookmark|
+  #     @movie_bookmark = MovieTag.create(movie: @movie, bookmark: bookmark)
+  #   end
 
-    redirect_to @list.movie, notice: 'Bookmark was created'
-    end
-  end
+  #   redirect_to @list.movie, notice: 'Bookmark was created'
+  #   end
+  # end
 
-  def destroy
-    @bookmark = Bookmark.find(params[:id])
-    @bookmark.destroy
-    redirect_to @list, notice: 'Bookmark was deleted.'
-  end
+  # def destroy
+  #   @bookmark = Bookmark.find(params[:id])
+  #   @bookmark.destroy
+  #   redirect_to @list, notice: 'Bookmark was deleted.'
+  # end
 
-  private
-    def bookmark_params
-      params.require(:bookmark).permit(:comment, :movie_id)
-    end
+  # private
+  #   def bookmark_params
+  #     params.require(:bookmark).permit(:comment, :movie_id)
+  #   end
 end
 
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,12 @@
 class MoviesController < ApplicationController
 
-  def index
-    @movies = Movie.all
-  end
+  # def index
+  #   @movies = Movie.all
+  # end
 
-  def show
-    @movie = Movie.find(params[:id])
-  end
+  # def show
+  #   @movie = Movie.find(params[:id])
+  # end
 
   # private
 
